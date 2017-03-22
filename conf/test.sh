d0999a52e75e2e678fbeea2643b9d182d69f4337
root <root@hamming.urgu.org>
2017-03-22 11:57:19 +0500

    Remove test config, it is not useful anymore.

--- conf/test.sh
+++
@@ -1,9 +0,0 @@
-METHOD=test
-DG_GROUPS=(asus)
-LOCAL_ADDRESS=212.193.68.251
-
-part() {
-    echo "/dev/disk/by-partlabel/$1"
-}
-LOCK=("/root/cow/conf/host/hamming.urgu.org.sh" "/root/xen/windows7.cfg")
-BAN=(asus1)
