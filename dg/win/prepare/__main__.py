04fb24a4b555526077b3abf5ce14408386179fe9
root <root@ritchie.urgu.org>
2019-10-21 18:49:43 +0500

    Add an option to create and switch snapshot symlinks, add "test mode".

--- dg/win/prepare/__main__.py
+++ dg/win/prepare/__main__.py
@@ -3,6 +3,7 @@ import contextlib2
 import datetime
 import logging
 import os
+import subprocess
 import sys
 
 from dg.win.prepare import lv
@@ -13,6 +14,7 @@ from dg.win.prepare import vm
 
 
 STARTUP_TIMEOUT = datetime.timedelta(minutes=5)
+SPECIALIZE_TIMEOUT = datetime.timedelta(minutes=15)
 
 
 def parse_args(raw_args):
@@ -29,6 +31,16 @@ def parse_args(raw_args):
                         help='Username for SSH connections, default is '
                              '"Administrator"')
 
+    parser.add_argument('-t', action='store_true',
+                        help='Test mode: start VM from the snapshot after '
+                             'sysprep and wait until it comes back up')
+    parser.add_argument('-l', metavar='LINK',
+                        help='Make a symlink from LINK to snapshot volume. '
+                             'If LINK already exists, it must be a symlink '
+                             'pointing to the snapshot done before, in which '
+                             'case it will be overwritten, and the old '
+                             'snapshot deleted')
+
     parser.add_argument('CONFIG', help='path to ref VM config file')
     parser.add_argument('HOST', help='host use for SSH connections')
 
@@ -42,8 +54,40 @@ def parse_args(raw_args):
     for path in (args.CONFIG, args.SYSPREP_XML):
         if not os.path.isfile(path):
             parser.error('{} does not exist'.format(path))
+    if args.l:
+        if args.t:
+            parser.error('test mode cannot be used with making snapshot '
+                         'symlink, please use either "-l" or "-t"')
+        if os.path.exists(args.l):
+            if not os.path.islink(args.l):
+                parser.error('{} is not a symbolic link'.format(args.l))
+            try:
+                subprocess.check_output(['lvdisplay', os.readlink(args.l)])
+            except subprocess.CalledProcessError:
+                parser.error('{} does not appear to be '
+                             'LVM logical volume'.format(args.l))
 
     return args
+
+
+def switch_symlink(link, new_snapshot):
+    if not link:
+        return
+
+    link_existed = os.path.exists(link)
+
+    if link_existed:
+        old_snapshot = os.readlink(link)
+        logging.info('Switching snapshot link at %s from %s to %s', link,
+                     old_snapshot, new_snapshot)
+        os.unlink(link)
+    else:
+        logging.info('Making snapshot link at %s to %s', link, new_snapshot)
+
+    os.symlink(new_snapshot, link)
+    if link_existed:
+        logging.info('Removing old snapshot %s', old_snapshot)
+        subprocess.check_call(['lvremove', '-f', old_snapshot])
 
 
 def main(raw_args):
@@ -60,13 +104,22 @@ def main(raw_args):
     with contextlib2.ExitStack() as stack:
         stack.enter_context(vm.xen_vm_shut_down(shutdown, args.CONFIG))
         disk_snapshot_name = stack.enter_context(
-            lv.snapshotted_volume(ref_vm_disk, args.s))
+            lv.snapshotted_volume(ref_vm_disk, args.s, clean=args.t))
         snapshot_vm_name = '{}-snapshot'.format(ref_vm_config['name'])
-        stack.enter_context(vm.xen_vm_with_alternate_disk(
-            args.CONFIG, snapshot_vm_name, args.d, disk_snapshot_name))
-        ssh_client.wait_ssh_ready(STARTUP_TIMEOUT)
-        sysprep.copy_setup_scripts(ssh_client, args.ss)
-        sysprep.start_sysprep(ssh_client, args.SYSPREP_XML)
+        with vm.xen_vm_with_alternate_disk(args.CONFIG, snapshot_vm_name,
+                                           args.d, disk_snapshot_name):
+            ssh_client.wait_ssh_ready(STARTUP_TIMEOUT)
+            sysprep.copy_setup_scripts(ssh_client, args.ss)
+            sysprep.start_sysprep(ssh_client, args.SYSPREP_XML)
+
+        if args.t:
+            logging.info('Starting VM from "sysprepped" snapshot for test')
+            with vm.xen_vm_with_alternate_disk(args.CONFIG, snapshot_vm_name,
+                                               args.d, disk_snapshot_name):
+                ssh_client.wait_ssh_ready(SPECIALIZE_TIMEOUT)
+                shutdown()
+        else:
+            switch_symlink(args.l, disk_snapshot_name)
 
 
 if __name__ == '__main__':
