e157438434de15a9353b84d70d80be3c7e7c9664
root <root@ritchie.urgu.org>
2023-07-03 03:02:30 +0500

    Add an option to scp back fro remote host.

--- dg/prepare/util/processes.py
+++ dg/prepare/util/processes.py
@@ -24,9 +24,9 @@ def ssh(host, command, output=False, options=None, method=subprocess.call,
         return log_and_call(cmdline, method=method, **kwargs)
 
 
-def scp(host, src, dst, options=None, **kwargs):
+def scp(host, src, dst, options=None, back=False, **kwargs):
     cmdline = ['scp']
     if options is not None:
         cmdline.extend(options)
-    cmdline.extend([src, f'{host}:{dst}'])
+    cmdline.extend([f'{host}:{src}', dst] if back else [src, f'{host}:{dst}'])
     return log_and_call(cmdline, method=subprocess.check_call, **kwargs)
