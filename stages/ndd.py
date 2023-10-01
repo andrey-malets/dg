a5d041fb14434bb2655e14d7a734a166911462ac
root <root@ritchie.urgu.org>
2023-10-02 00:03:27 +0500

    Increase verbosity for ndd.py command.

--- stages/ndd.py
+++ stages/ndd.py
@@ -56,7 +56,7 @@ class RunNDD(config.WithLocalAddress, config.WithNDDArgs, config.WithConfigURL,
             with self.prepared_input(spec.input_, spec.iargs, state.log) \
                     as input_:
                 cmdline = [
-                    '/usr/local/bin/ndd.py', '-v', '-p', self.ndd_port,
+                    '/usr/local/bin/ndd.py', '-vv', '-p', self.ndd_port,
                     '-i', input_, '-o', spec.output
                 ]
 
